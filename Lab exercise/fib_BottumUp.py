6179b5095023d4302dbe182a0b349c8d6a818c0e
Akhilesh gowda <akhileshgowda7@gmail.com>
2021-11-04 13:44:08 -0400

    Fibonacci series using Bottom up approach

---
+++ Lab exercise/fib_BottumUp.py
@@ -0,0 +1,14 @@
+def fib_bottomUp(n):
+    if n==1 or n==2:
+        return 1
+
+    bottom_up = [0]*(n+1)
+    bottom_up[1]=1
+    bottom_up[2]=1
+
+    for i in range (3,n+1):
+        bottom_up[i]=bottom_up[i-1]+bottom_up[i-2]
+
+    return bottom_up[n]
+
+print(fib_bottomUp(55))
