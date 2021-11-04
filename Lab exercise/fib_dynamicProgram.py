8f5651c76065ebf4e3d0319aaa781a9efdd1e4b9
Akhilesh gowda <akhileshgowda7@gmail.com>
2021-11-04 13:42:17 -0400

    Fibonacci Series using dynamic programming

---
+++ Lab exercise/fib_dynamicProgram.py
@@ -0,0 +1,18 @@
+def fib(n,memo):
+    if memo[n] is not None:
+        return memo[n]
+    if n==1 or n==2:
+        result = 1
+    else:
+        result = fib(n-1,memo)+fib(n-2,memo)
+
+    memo[n]=result
+    return result
+
+def fib_dynamic(n):
+    memo=[None]*(n+1)
+    return fib(n,memo)
+
+
+print(fib_dynamic(55))
+
